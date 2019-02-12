8c567af4f7ce06d0793f9d92216d2678401528f6
Maddy Tyler <tyler.madeleine@gmail.com>
2019-02-12 18:32:33 -0500

    Update ServerTesting.java

--- Server.java
+++ Server.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Server {
 
   DatagramSocket serverSocket;
@@ -8,36 +13,47 @@ public class Server {
   byte[] sendData;
 
   /**
-   * Sets up the server so it can receive messages from a specified port
-   * @param portNum
-   */
+  * Sets up the server so it can receive messages from a specified port
+  * @param portNum
+  */
   public Server(int portNum) {
-    serverSocket = new DatagramSocket(portNum);
-    receiveData = new byte[255];
-    sendData = new byte[255];
+    try {
+      serverSocket = new DatagramSocket(portNum);
+      receiveData = new byte[255];
+      sendData = new byte[255];
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
   /**
-   * Starts the server listening on its port
-   */
+  * Starts the server listening on its port
+  */
   public void run() {
 
     while(true) {
-      //initiliaze DatagramPacket to recieve data from the Client
-      receivePacket = new DatagramPacket(receiveData, receiveData.length);
-      // receive data
-      serverSocket.receive(receivePacket);
-      String message = new String(receivePacket.getData());
-      //check for closing request - do we respond if we get closing request?
-      if(message == "close")
-        break;
-      //sending response
-      InetAddress address = receivePacket.getAddress();
-      int port = receivePacket.getPort();
-      //What do we reply?
-      String reply = "Reply";
-      sendData = reply.getBytes();
-      sendPacket = new DatagramPacket(sendData, sendData.length, address, port);
-      serverSocket.send(sendPacket);
+      System.out.println("Server running");
+      try {
+        //initiliaze DatagramPacket to recieve data from the Client
+        receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        // receive data
+        serverSocket.receive(receivePacket);
+        String message = new String(receivePacket.getData());
+        //check for closing request - do we respond if we get closing request?
+        if(message == "close")
+        {
+          break;
+        }
+        //sending response
+        InetAddress address = receivePacket.getAddress();
+        int port = receivePacket.getPort();
+        //What do we reply?
+        String reply = "Reply";
+        sendData = reply.getBytes();
+        sendPacket = new DatagramPacket(sendData, sendData.length, address, port);
+        serverSocket.send(sendPacket);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
     serverSocket.close();
   }
---
+++ ServerTesting.java
@@ -0,0 +1,7 @@
+public class ServerTesting {
+
+  public static void main(String[] args) {
+    Server server = new Server(4445);
+    server.run();
+  }
+}
