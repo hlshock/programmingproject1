a4f24d2698f9125c5c532ae8b41b693f826713d7
Maddy Tyler <tyler.madeleine@gmail.com>
2019-02-12 18:02:30 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,3 +1,30 @@
 public class Server {
 
+  DatagramSocket serverSocket;
+  DatagramPacket receivePacket;
+  DatagramPacket sendPacket;
+
+  /**
+   * Sets up the server so it can receive messages from a specified port
+   * @param portNum
+   */
+  public Server(int portNum) {
+    serverSocket = new DatagramSocket(portNum);
+  }
+
+  public void run() {
+    receivePacket = new DatagramPacket();
+    sendPacket = new DatagramPacket();
+    while(true) {
+      //initiliaze DatagramPacket to recieve data from the Client
+      receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      // receive data
+      serverSocket.receive(receivePacket);
+      String message = new String(receivePacket.getData());
+      //check for closing request
+      if(message == "close")
+        break;
+    }
+    serverSocket.close();
+  }
 }
