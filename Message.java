5c99797ce5e7839772a2e3d09b2aafbbd6f3a818
Maddy Tyler <tyler.madeleine@gmail.com>
2019-02-12 18:57:26 -0500

    Update Message.java

--- Message.java
+++ Message.java
@@ -14,9 +14,23 @@ public class Message {
   public Message(byte[] messageBytes) {
 
   }
-
+  public int getSequenceCounter() {
+    return sequenceCounter;
+  }
+  public void setSequenceCounter(int counter) {
+    sequenceCounter = counter;
+  }
+  public String getMessageContents() {
+    return messageContents;
+  }
+  public void setSequenceCounter(String message) {
+    messageContents = message;
+  }
+  /**
+   * @return the byte array representation of this Message object
+   */
   public byte[] getBytes() {
-    
+
   }
 
 
