cb28e7482b3eb80b4544333576e3fad133782244
Maddy Tyler <tyler.madeleine@gmail.com>
2019-02-17 17:21:59 -0500

    update

--- NetworkTesting.java
+++ NetworkTesting.java
@@ -2,7 +2,7 @@ public class NetworkTesting {
 
   public static void main(String[] args) {
     System.out.println("Network Testing:");
-    UnreliableNetwork network = new UnreliableNetwork(4445, 4446);
+    UnreliableNetwork network = new UnreliableNetwork(4446, 4445);
     network.run();
   }
 }
--- UnreliableNetwork.java
+++ UnreliableNetwork.java
@@ -4,8 +4,7 @@ import java.util.Random;
 
 public class UnreliableNetwork {
 
-  DatagramSocket clientSocket;
-  DatagramSocket serverSocket;
+  DatagramSocket networkSocket;
 
   DatagramPacket receivePacket;
   DatagramPacket sendPacket;
@@ -18,16 +17,15 @@ public class UnreliableNetwork {
 
   boolean testing = true;
 
-  public UnreliableNetwork(int clientPortNum, int serverPortNum) {
+  public UnreliableNetwork(int cNum, int sNum) {
     if(testing) {
       System.out.println("Unreliable Network running...");
     }
-    this.clientPortNum = clientPortNum;
-    this.serverPortNum = serverPortNum;
+    clientPortNum = cNum;
+    serverPortNum = sNum;
     //what port number do we use?
     try {
-      clientSocket = new DatagramSocket(clientPortNum);
-      serverSocket = new DatagramSocket(serverPortNum);
+      networkSocket = new DatagramSocket(clientPortNum);
     } catch (Exception e)
     {
       e.printStackTrace();
@@ -36,29 +34,39 @@ public class UnreliableNetwork {
   public void run() {
     Random rand = new Random();
     boolean continueReceiving = true;
+    if(testing) {
+      System.out.println("Waiting for messages...");
+    }
     //continue looping until we get end message
     while(continueReceiving)
     {
-      if(testing) {
-        System.out.println("Waiting for messages...");
-      }
       receiveData = new byte[256];
       sendData = new byte[256];
       try {
+        //receive message from Client:
         receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        clientSocket.receive(receivePacket);
+        //connect to port number of client socket
+        networkSocket.connect(InetAddress.getByName("localhost"), clientPortNum);
+        networkSocket.receive(receivePacket);
         if(testing) {
           System.out.println("Message received...");
         }
         //send message half the time
         if(rand.nextInt(11) < 6) {
           if(testing) {
-            System.out.println("Forwarding message...");
+            System.out.println("Forwarding Packet...");
           }
           sendData = receivePacket.getData();
           //not sure what address to use
+          networkSocket.connect(InetAddress.getByName("localhost"), serverPortNum);
           sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("localhost"), serverPortNum);
-          serverSocket.send(sendPacket);
+          networkSocket.send(sendPacket);
+        }
+        else
+        {
+          if(testing) {
+            System.out.println("Packet dropped...");
+          }
         }
 
       } catch (Exception e) {
@@ -68,8 +76,7 @@ public class UnreliableNetwork {
   }
 
   public void close() {
-    clientSocket.close();
-    serverSocket.close();
+    networkSocket.close();
   }
 
 }
